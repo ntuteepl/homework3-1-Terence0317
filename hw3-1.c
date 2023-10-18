b7c02bbe8541e49fb59e8ca5add1fe445803c49b
林佑恩 <146064642+Terence0317@users.noreply.github.com>
2023-10-18 12:38:20 +0800

    Update hw3-1.c

--- hw3-1.c
+++ hw3-1.c
@@ -1,1 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+int main()
+{
+    int n,i;
+    scanf("%d", &n);
+    if (n > 0){
+        if (n == 1) printf("NO");
+        else if (n == 2) printf("YES");
+        else{
+            for (i=2;i<n;i++){
+                if (n % i == 0){
+                    printf("NO");
+                    break;
+                }
+                else if (i==n-1) printf("YES");
+            }
+        }
+    }
+}
 
