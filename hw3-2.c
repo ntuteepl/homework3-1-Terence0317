e5bb6715198b9a1589c27bcde9d0dff178e59519
林佑恩 <146064642+Terence0317@users.noreply.github.com>
2023-10-23 21:14:47 +0800

    Update hw3-2.c

--- hw3-2.c
+++ hw3-2.c
@@ -12,8 +12,8 @@ int main(){
         else if (d2 < s3 && d3 < s1) car -= 2; // 2,3,1
         else if (d1 < s3 && d3 < s2) car -= 2; // 1,3,2
         else if (d3 < s1 && d1 < s2) car -= 2; // 3,1,2
-        else if ( (s1 < s2 && s2 < s3) && (d1 > s2 && d2 > s3) ) car = 3;
-        else if ( (s3 < s2 && s2 < s1) && (d3 > s2 && d2 > s1) ) car = 3;
+        else if ( (s1 < s2 && s2 < s3) && (d1 > s2 && d2 > s3 && d1 > s3) ) car = 3;
+        else if ( (s3 < s2 && s2 < s1) && (d3 > s2 && d2 > s1 && d1 > s1) ) car = 3;
         else car -= 1;
 
     printf("%d", car);
