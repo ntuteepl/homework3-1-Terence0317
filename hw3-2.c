292d4c773942d4ea4ab6603c70b479db5c74c46b
林佑恩 <146064642+Terence0317@users.noreply.github.com>
2023-10-23 20:53:31 +0800

    Update hw3-2.c

--- hw3-2.c
+++ hw3-2.c
@@ -2,16 +2,20 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
-int main()
-{
-    int s1, d1, s2, d2, s3, d3,car;
+int main(){
+    int s1, d1, s2, d2, s3, d3, car = 3;
     scanf("%d%d%d%d%d%d", &s1, &d1, &s2, &d2, &s3, &d3);
     if (s1 > 0 && s1 < d1 && d1 <= 24 && s2 > 0 && s2 < d2 && d2 <= 24 && s3 > 0 && s3 < d3 && d3 <= 24){
-        if (s1 < s2 && s2 < s3){
-            if (d1 < s2 && d2 < s3) car = 1;
-            else if ((d1 < s2 && d2 >= s3) || (d1 >= s2 && d2 < s3) || (d1 < s3 && d2 >= s3)) car = 2;
-            else car = 3;
-        }
+        if (d1 < s2 && d2 < s3) car -= 2; //1,2,3
+        else if (d3 < s2 && d2 < s1) car -= 2; //3,2,1
+        else if (d2 < s1 && d1 < s3) car -= 2; // 2,1,3
+        else if (d2 < s3 && d3 < s1) car -= 2; // 2,3,1
+        else if (d1 < s3 && d3 < s2) car -= 2; // 1,3,2
+        else if (d3 < s1 && d1 < s2) car -= 2; // 3,1,2
+        else if ( (s1 < s2 && s2 < s3) && (d1 > s2 && d2 > s3) ) car = 3;
+        else if ( (s3 < s2 && s2 < s1) && (d3 > s2 && d2 > s1) ) car = 3;
+        else car -= 1;
+
     printf("%d", car);
     }
 }
