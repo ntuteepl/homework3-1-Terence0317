225fd9e04ad53fa487a70d26692c17bdff3d37a6
林佑恩 <146064642+Terence0317@users.noreply.github.com>
2023-10-23 20:00:01 +0800

    Update hw3-2.c

--- hw3-2.c
+++ hw3-2.c
@@ -4,23 +4,14 @@
 
 int main()
 {
-    int s1, d1, s2, d2, s3, d3,car = 3;
+    int s1, d1, s2, d2, s3, d3,car;
     scanf("%d%d%d%d%d%d", &s1, &d1, &s2, &d2, &s3, &d3);
     if (s1 > 0 && s1 < d1 && d1 <= 24 && s2 > 0 && s2 < d2 && d2 <= 24 && s3 > 0 && s3 < d3 && d3 <= 24){
         if (s1 < s2 && s2 < s3){
-            if (d1 <= s2 || d1 <= s3){
-                if (d2 <= s3) car = 1;
-                else car -= 1;
-            }
-            else if (d1 < d2 && d2 < d3) car = 3;
+            if (d1 < s2 && d2 < s3) car = 1;
+            else if ((d1 < s2 && d2 >= s3) || (d1 >= s2 && d2 < s3) || (d1 < s3 && d2 >= s3)) car = 2;
+            else car = 3;
         }
-        else{
-            if (d1 <= s2 || d1 <= s3){
-                if (d2 <= s3) car = 1;
-                else car -= 1;
-            }
-            else car = 1;
-        }
-    }
     printf("%d", car);
+    }
 }
